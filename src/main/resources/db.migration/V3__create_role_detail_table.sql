ead0125ab1e63db9905104defcaf2d5b73572a31
Joshua Noble <joshua.noble@kainos.com>
2024-07-26 13:57:16 +0100

    new migration scripts

--- src/main/resources/db.migration/V3__create_role_detail_table.sql
+++ src/main/resources/db.migration/V3__create_role_detail_table.sql
@@ -1,9 +1,7 @@
 CREATE TABLE Role_Detail (
-	`RoleDetailId` smallint PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `roleName` varchar(100) NOT NULL,
+	`roleDetailId` smallint PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `detailName` varchar(100) NOT NULL,
     `description` varchar(7500) NOT NULL,
     `responsibilities` varchar(3500) NOT NULL,
-    `link` varchar(1000) NOT NULL,
-    RoleId int NOT NULL,
-    FOREIGN KEY (RoleId) REFERENCES Role (RoleId)
+    `link` varchar(1000) NOT NULL
 );
--- src/main/resources/db.migration/V4__adding_role_detail_data.sql
+++ src/main/resources/db.migration/V4__adding_role_detail_data.sql
@@ -1,2 +1,5 @@
-INSERT INTO Role (`roleName`,`location`,`capability`,`band`,`closingDate`,`status`)
-VALUES ('Engineer','hillsborough','large','marching',DATE '2015-12-17','open');
+INSERT INTO Role_Detail (`detailName`,`description`,`responsibilities`,`link`)
+VALUES ('Plumber - big strong','Belfast plumbing service','making sure sewers are clean','http://plumbing-belfast.com');
+
+INSERT INTO Role_Detail (`detailName`,`description`,`responsibilities`,`link`)
+VALUES ('lawyer','Lisburn lawyering','make sure you defend them','http://lawyer-lisburn.com');
