40777e281205fb774b702f2fd1813ec88c134ae8
joel-and <joel.anderson@kainos.com>
2024-08-07 09:27:57 +0100

    fixed integration tests- passing locally

--- src/test/java/integration/JobRoleIntegrationTest.java
+++ src/test/java/integration/JobRoleIntegrationTest.java
@@ -5,11 +5,15 @@ import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.example.TestApplication;
 import org.example.TestConfiguration;
 import org.example.models.JobRole;
+import org.example.models.LoginRequest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
@@ -17,6 +21,24 @@ import java.util.List;
 public class JobRoleIntegrationTest {
     private static final DropwizardAppExtension<TestConfiguration> APP =
             new DropwizardAppExtension<>(TestApplication.class);
+    private static String token;
+
+    @BeforeAll
+    public static void setUp() {
+        Client client = APP.client();
+
+        String adminUsername = System.getenv().get("ADMIN_USERNAME");
+        String adminPassword = System.getenv().get("ADMIN_PASSWORD");
+
+        LoginRequest loginRequest = new LoginRequest(adminUsername, adminPassword);
+
+        Response response = client
+                .target("http://localhost:8080/api/auth/login")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(loginRequest, MediaType.APPLICATION_JSON));
+
+        token = response.readEntity(String.class);
+    }
 
     @Test
     void getJobRoles_ShouldReturnListOfJobRoles() {
@@ -25,6 +47,7 @@ public class JobRoleIntegrationTest {
         List<JobRole> response = client
                 .target("http://localhost:8080/api/job-roles")
                 .request()
+                .header("Authorization", "Bearer " + token)
                 .get(List.class);
 
         Assertions.assertFalse(response.isEmpty());
@@ -37,6 +60,7 @@ public class JobRoleIntegrationTest {
         Response response = client
                 .target("http://localhost:8080/api/job-roles/1")
                 .request()
+                .header("Authorization", "Bearer " + token)
                 .get();
 
         Assertions.assertEquals(200, response.getStatus());
@@ -50,6 +74,7 @@ public class JobRoleIntegrationTest {
         int response = client
                 .target("http://localhost:8080/hr/employee/123456")
                 .request()
+                .header("Authorization", "Bearer " + token)
                 .get()
                 .getStatus();
 
